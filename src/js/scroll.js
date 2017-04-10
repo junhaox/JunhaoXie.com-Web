cf8ccefdedc5a96faedf3a8e4a7b8c4842c008da
Junhao Xie <junhaox.k@gmail.com>
2017-04-10 15:44:01 -0700

    Add smooth scroll using jQuery

---
+++ src/js/scroll.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+})
