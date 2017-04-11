3c37ead75257839882230d59a803cb021b4b4a13
Junhao Xie <junhaox.k@gmail.com>
2017-04-10 17:48:44 -0700

    cleanup

---
+++ src/js/api.js
@@ -0,0 +1,7 @@
+function myMap() {
+    var myCenter = new google.maps.LatLng(32.8632119,-117.2127723);
+    var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
+}
